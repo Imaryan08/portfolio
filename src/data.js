2bad612073955fd534cc9695a24ded6836424fbe
Rampukar <imaryan08@gmail.com>
2022-04-27 21:36:19 +0530

    portfolio

---
+++ src/data.js
@@ -0,0 +1,59 @@
+const skillsList = [
+    { title: "React JS", value: 75 },
+    { title: "CSS", value: 80 },
+    { title: "BootStrap", value: 80 },
+    { title: "HTML", value: 80 },
+    { title: "Node JS ", value: 60 },
+    { title: "Express JS ", value: 70 },
+    { title: "MongoDB", value: 70 },
+    { title: "NPM", value: 70 },
+];
+const projectList = [
+    {
+        id: 1,
+        title: "Blinkit",
+        technologies: ["JavaScript", "Node JS", "MongoDB"],
+        backgroundImage:
+            "https://cdn.cbeditz.com/cbeditz/preview/black-red-gradient-background-wallpaper-74-11614352798fbqrv1wpuv.jpg",
+        frontImage: "https://i.ibb.co/L0F7mKX/tunflix.png",
+    },
+    {
+        id: 2,
+        title: "amazon prime clone",
+        technologies: ["HTML", "CSS", "JavaScript", "BootStrap"],
+        backgroundImage:
+            "https://img.freepik.com/free-vector/dark-gradient-background-with-copy-space_53876-99548.jpg?size=626&ext=jpg&ga=GA1.2.2102900112.1628985600",
+        frontImage: "https://i.ibb.co/zNMJFTW/Webp-net-resizeimage.png",
+    },
+    {
+        id: 3,
+        title: "Burpees.io",
+        technologies: ["React JS", "Node JS", "MongoDB"],
+        backgroundImage:
+            "https://media.istockphoto.com/vectors/abstract-purple-vector-background-with-stripes-vector-id972475894?k=6&m=972475894&s=612x612&w=0&h=99AirGMOb64N2-1ZSMYRjEBp2USrAdzXUGzQMh5o6Js=",
+        frontImage: "https://themes-backend.material-ui.com/wp-content/uploads/2021/05/01_preview.jpg",
+    },
+];
+
+const experienceList = [
+  {
+    id: 0,
+    company: "masai",
+    links: {
+      website: "https://www.masaischool.com",
+      facebook: "https://www.facebook.com/masaischool/",
+      instagram: "https://www.instagram.com/masaischool/",
+    },
+  },
+  {
+    id: 1,
+    company: "primezone",
+    links: {
+      website: "https://www.primezoneinfotech.com/",
+      facebook: "https://www.facebook.com/primezoneinfotech/",
+      instagram: "https://twitter.com/primezoneit/",
+    },
+  },
+];
+
+export { skillsList, projectList, experienceList };
